794ce26c5cbbcae6cdd8007853896f630bc827c7
Melinda Mezinca <melinda.mezinca@yahoo.com>
2018-11-25 01:40:24 +0200

    Adds person generator API + aggregation API

--- node-js/src/routes/persons.js
+++ node-js/src/routes/persons.js
@@ -17,7 +17,21 @@ module.exports = {
             method: 'GET',
             path: '/dbHealth',
             handler: async (request, h) => {
-                return service.getDbConnectionHealth(options.dbUrl, options.db);
+                return service.getDbConnectionHealth(options.db).then(result=> h.response(result).code(200));
+            }
+        });
+        server.route({
+            method: 'POST',
+            path: '/person',
+            handler: async (request, h) => {
+                return service.generatePersons(options.db).then(result=> h.response({result: result}).code(201));
+            }
+        });
+        server.route({
+            method: 'GET',
+            path: '/personAggregation',
+            handler: async (request, h) => {
+                return service.aggregateAttributes(options.db).then(result=> h.response({counter: result}).code(200));
             }
         });
     }
--- node-js/src/services/personsService.js
+++ node-js/src/services/personsService.js
@@ -1,11 +1,12 @@
 // get, post, aggregate
 'use strict';
+const faker = require('faker');
 
 module.exports = {
-    getDbConnectionHealth: async (url, client) => {
+    getDbConnectionHealth: async (client) => {
         try {
-        const db = client.db('admin');
-         return new Promise((resolve, reject) => db.collection('test').findOne({}, (err) => {
+            const db = client.db('admin');
+            return new Promise((resolve, reject) => db.collection('test').findOne({}, (err) => {
                 if (err == null) {
                     return resolve({ status: 'OK' });
                 }
@@ -16,6 +17,43 @@ module.exports = {
         } catch (err) {
             console.log(err);
             return { status: 'DOWN' };
+        }
+    },
+    generatePersons: async (client) => {
+        try {
+            const db = client.db('admin');
+            return new Promise((resolve, reject) => {
+                for (let i = 0; i < 10; i++) {
+                    db.collection('test').insertOne({ personName: faker.name.findName(), age: Math.floor(Math.random() * 10 + 20) }, (err) => {
+                        if (err == null) {
+                            return resolve('10 new persons inserted');
+                        }
+                        else {
+                            console.log('An error occured while saving the data', err);
+                            return reject(err);
+                        }
+                    })
+                }
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    },
+    aggregateAttributes: async (client) => {
+        try {
+            const db = client.db('admin');
+            return new Promise((resolve, reject) => {
+                db.collection('test').countDocuments({ age: { $gt: 25 } }, (err, result) => {
+                    if (err == null) {
+                        return resolve(result);
+                    }
+                    if (err != null) {
+                        return reject(err);
+                    }
+                })
+            });
+        } catch (err) {
+            console.log(err);
         }
     }
 }
